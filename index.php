ef7e4c967a63a28d7db01fb7d765def60b5a04b4
Anthony Nguyen <anthonydtnguyen@gmail.com>
2019-07-26 09:20:45 -0700

    Update index.php
    adjust form tag

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
 			Today's Date:<input type = "date" name = "returnDate"><br>
 			<input type="submit" value="Return">
 		</form>
-		<form>Filter Input:<input type="text" id="filterInputStr" oninput="filterOnInput('filterInputStr')"><br></form>
+		<form>Filter Input:<input type="text" id="filterInputStr" oninput="filterOnInput('filterInputStr')"></form><br>
 		<?php 
 			if(isset($_GET['checkout'])){
 				$result= $_GET['checkout'];
