035ea76fc7dbac21a33b60601eeae2dbe157b22f
Anthony Nguyen <anthonydtnguyen@gmail.com>
2019-08-02 10:26:39 -0700

    adding header container

--- backend.php
+++ backend.php
@@ -19,7 +19,7 @@ $result = $conn->query($sql);
 $pcnN = 0;
 $serialN = 0;
 if ($result->num_rows > 0) {
-	echo "<div id = tableContainer><table><tr><th onclick = 'sortTable(0)'>Manufacture & Model</th><th onclick = 'sortTable(1)'>Description</th><th onclick = 'sortTable(2)'>PCN</th><th onclick = 'sortTable(3)'>Serial#</th><th onclick = 'sortTable(4)'>Calibration Due Date</th><th onclick = 'sortTable(5)'>Location of Item</th><th onclick = 'sortTable(6)'>Last Checked Out By</th><th onclick = 'sortTable(7)'>Checked Out On</th><th onclick = 'sortTable(8)'>Returned On</th></tr></table></div>";
+	echo "<div id = headerContainer><table><tr><th onclick = 'sortTable(0)'>Manufacture & Model</th><th onclick = 'sortTable(1)'>Description</th><th onclick = 'sortTable(2)'>PCN</th><th onclick = 'sortTable(3)'>Serial#</th><th onclick = 'sortTable(4)'>Calibration Due Date</th><th onclick = 'sortTable(5)'>Location of Item</th><th onclick = 'sortTable(6)'>Last Checked Out By</th><th onclick = 'sortTable(7)'>Checked Out On</th><th onclick = 'sortTable(8)'>Returned On</th></tr></table></div>";
     echo "<div id = tableContainer><table id = 'myTable'><tr><th onclick = 'sortTable(0)'>Manufacture & Model</th><th onclick = 'sortTable(1)'>Description</th><th onclick = 'sortTable(2)'>PCN</th><th onclick = 'sortTable(3)'>Serial#</th><th onclick = 'sortTable(4)'>Calibration Due Date</th><th onclick = 'sortTable(5)'>Location of Item</th><th onclick = 'sortTable(6)'>Last Checked Out By</th><th onclick = 'sortTable(7)'>Checked Out On</th><th onclick = 'sortTable(8)'>Returned On</th></tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
