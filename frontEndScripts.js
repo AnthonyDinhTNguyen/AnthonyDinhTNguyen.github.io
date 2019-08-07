df4f3435aa0c7bb61e807b6de84d6e69a08a28df
Anthony Nguyen <anthonydtnguyen@gmail.com>
2019-08-07 08:35:02 -0700

    Update frontEndScripts.js
    clear selection revision

--- frontEndScripts.js
+++ frontEndScripts.js
@@ -193,14 +193,17 @@ function pasteOnClick(pcnOrSerial,formNum){
 }
 
 function clearSelection(){
-  var sel = window.getSelection ? window.getSelection() : document.selection;
-  if (sel) {
-    if (sel.removeAllRanges) {
-        sel.removeAllRanges();
-    } else if (sel.empty) {
-        sel.empty();
+  if (window.getSelection) {
+    if (window.getSelection().empty) {  // Chrome
+      window.getSelection().empty();
+    } 
+    else if (window.getSelection().removeAllRanges) {  // Firefox
+      window.getSelection().removeAllRanges();
     }
-}
+  }
+  else if (document.selection) {  // IE?
+    document.selection.empty();
+  }
 }
 function copyOnClick(n){
   //figure out which form checkout, return, update... is visible
