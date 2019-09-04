e58cf0da9451a07943d3cc63f09e536dd36ca94e
Anthony Nguyen <anthonydtnguyen@gmail.com>
2019-09-04 10:00:14 -0700

    adding restore to mssql
    adding establish connection as a separate file

---
+++ establishConnection.php
@@ -0,0 +1,21 @@
+<html>
+<script src="frontEndScripts.js"></script>
+<?php
+$servername = "den1.mssql8.gear.host";
+$username = "moogmssql";
+$password = "Ju0UnRH6?VI~";
+$dbname = "moogmssql";
+
+// Create connection
+$connectionInfo = array("Database"=>"moogmssql","UID"=>"moogmssql","PWD"=>"Ju0UnRH6?VI~");
+$conn = sqlsrv_connect($servername, $connectionInfo);
+// Check connection
+/*if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} */
+if( $conn ) {
+     echo "Connection established.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
--- restore.php
+++ restore.php
@@ -1,15 +1,14 @@
+<?php include 'establishConnection.php';?>
 <?php
-
-$servername = "den1.mysql6.gear.host";
+/*$servername = "den1.mysql6.gear.host";
 $username = "inventorymoog";
 $password = "Ti6d-o4_bwPf";
 $dbname = "inventorymoog";
-
 $headerrow = 0;
-$connect = mysqli_connect($servername, $username, $password, $dbname);
+$connect = mysqli_connect($servername, $username, $password, $dbname);*/
 if(isset($_POST["submit"]))
 {
-	mysqli_query($connect,"DELETE FROM ESDInventory");
+	sqlsrv_query($conn,"DELETE FROM ESDInventory");
 	if($_FILES['file']['name'])
 	{
 		$filename = explode(".", $_FILES['file']['name']);
@@ -22,7 +21,7 @@ if(isset($_POST["submit"]))
 					$headerrow = 1;
 				}
 				else{
-					$model = mysqli_real_escape_string($connect, $data[0]);  
+					/*$model = mysqli_real_escape_string($connect, $data[0]);  
 					$description = mysqli_real_escape_string($connect, $data[1]);
 					$pcn = mysqli_real_escape_string($connect, $data[2]);
 					$serial = mysqli_real_escape_string($connect, $data[3]);
@@ -30,9 +29,18 @@ if(isset($_POST["submit"]))
 					$area = mysqli_real_escape_string($connect, $data[4]);
 					$name = mysqli_real_escape_string($connect, $data[6]);
 					$checkoutDate = mysqli_real_escape_string($connect, $data[7]);
-					$returnDate = mysqli_real_escape_string($connect, $data[8]);
+					$returnDate = mysqli_real_escape_string($connect, $data[8]);*/
+					$model = $data[0];  
+					$description = $data[1];
+					$pcn = $data[2];
+					$serial = $data[3];
+					$calibration = $data[5];
+					$area = $data[4];
+					$name = $data[6]);
+					$checkoutDate = $data[7];
+					$returnDate = $data[8];
 					$query = "INSERT into ESDInventory(model, description, pcn, serial, area, calibration, name, checkoutDate, returnDate) values('$model','$description','$pcn','$serial','$area','$calibration','$name','$checkoutDate', '$returnDate')";
-					mysqli_query($connect, $query);
+					sqlsrv_query($conn, $query);
 				}
 			}
 			fclose($handle);
@@ -42,5 +50,5 @@ if(isset($_POST["submit"]))
 }
 	
 header("Location: managementPage.php");
-mysqli_close($connect);
+sqlsrv_close($conn);
 ?>
