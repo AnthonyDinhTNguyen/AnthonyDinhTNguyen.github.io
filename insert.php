517ce3267df607c615471e3c6803b27c38850d32
Anthony Nguyen <anthonydtnguyen@gmail.com>
2019-09-04 10:39:59 -0700

    Update insert.php
    trying insert

--- insert.php
+++ insert.php
@@ -1,6 +1,6 @@
 <?php
-
-$servername = "den1.mysql6.gear.host";
+include 'establishConnection.php';
+/*$servername = "den1.mysql6.gear.host";
 $username = "inventorymoog";
 $password = "Ti6d-o4_bwPf";
 $dbname = "inventorymoog";
@@ -10,10 +10,10 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+} */
 //Prepare Statement
-$stmt = $conn->prepare("INSERT INTO ESDInventory (model, description, pcn, serial, area, calibration) VALUES (?,?,?,?,?,?)");
-$stmt->bind_param("ssssss", $model, $description, $pcn, $serial, $area, $calibration);
+
+//$stmt->bind_param("ssssss", $model, $description, $pcn, $serial, $area, $calibration);
 
 $model = filter_input(INPUT_POST,'model');
 $description = filter_input(INPUT_POST,'description');
@@ -24,12 +24,10 @@ $calibration = filter_input(INPUT_POST,'calibration');
 
 if(empty($pcn)&&empty($serial)){
 	header("Location: managementPage.php?insert=*FAILED to Insert Item. Please Enter a Serial Number or PCN*");
-	$stmt->close();
-	$conn->close();
+	sqlsrv_close($conn);
 	exit();
 }
-$stmt->execute();
-$stmt->close();
+$result = sqlsrv_query($conn, "INSERT INTO ESDInventory (model, description, pcn, serial, area, calibration) VALUES (?,?,?,?,?,?)", [$model,$description,$pcn,$serial,$area,$calibration,$result]);
 header("Location: managementPage.php?insert=*SUCCESSFULLY Inserted Item*");
-$conn->close();
+sqlsrv_close($conn);
 ?>
