6731f11bc0929d931062eddd12ce169d3dc69af0
Anthony Nguyen <anthonydtnguyen@gmail.com>
2019-09-04 12:39:40 -0700

    Update backup.php

--- backup.php
+++ backup.php
@@ -6,21 +6,20 @@ $result = sqlsrv_query($conn, 'SELECT * FROM ESDInventory');
 if ($result==false) die('Couldn\'t fetch records');
 $num_fields = sqlsrv_num_fields($result);
 $headers = array();
-$fieldinfo = sqlsrv_field_metadata($result)
+$fieldinfo = sqlsrv_field_metadata($result);
     for($i = 0; $i <count($fieldinfo); $i++)
         $headers[$i] = $fieldinfo[$i]["Name"];
-echo "done";
-/*$fp = fopen('php://output', 'w');
+$fp = fopen('php://output', 'w');
 if ($fp && $result) {
 	$fn = filter_input(INPUT_POST,'filename').".csv";
     header('Content-Type: text/csv');
     header("Content-Disposition: inline; filename=$fn");
     header('Pragma: no-cache');
     header('Expires: 0');
-    fputcsv($fp, array_values($headers));
+    fputcsv($fp, $headers);
     while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_NUMERIC)) {
         fputcsv($fp, array_values($row));
     }
     die;
-}*/
+}
 ?>
