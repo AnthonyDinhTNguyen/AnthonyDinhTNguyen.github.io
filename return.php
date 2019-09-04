60174ae6cc30cd3d0bb1415aa239b247d1489e51
Anthony Nguyen <anthonydtnguyen@gmail.com>
2019-09-04 11:25:36 -0700

    Update return.php
    return

--- return.php
+++ return.php
@@ -1,35 +1,19 @@
 <?php
-
-$servername = "den1.mysql6.gear.host";
-$username = "inventorymoog";
-$password = "Ti6d-o4_bwPf";
-$dbname = "inventorymoog";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+include 'establishConnection.php';
 //Prepare Statement
-$stmt = $conn->prepare("UPDATE ESDInventory SET area = 'ESD LAB', returnDate = ? WHERE serial = ? OR pcn = ?");
-$stmt->bind_param("sss",$returnDate, $serial, $pcn);
 
 $pcn = trim(filter_input(INPUT_POST,'pcn'));
 $serial = trim(filter_input(INPUT_POST,'serial'));
 $returnDate = filter_input(INPUT_POST,'returnDate');
-
 $continue = true;
 if(empty($returnDate)){
 	header("Location: index.php?return=*FAILED to return item. Please Enter The Return Date*");
-	$stmt->close();
-	$conn->close();
+	sqlsrv_close($conn);
 	exit();
 }
 elseif(empty($pcn)&&empty($serial)){
 	header("Location: index.php?return=*FAILED to Return Item. Enter either a PCN or Serial Number*");
-	$stmt->close();
-	$conn->close();
+	sqlsrv_close($conn);
 	exit();
 }
 elseif(empty($pcn)){
@@ -38,17 +22,16 @@ elseif(empty($pcn)){
 elseif(empty($serial)){
 	$serial = "TEMP NAME TO PREVENT...";
 }
-$stmt->execute();
-if($stmt->affected_rows>=1){
+//$stmt->execute();
+$stmt = sqlsrv_query($conn,"UPDATE ESDInventory SET area = 'ESD LAB', returnDate = ? WHERE serial = ? OR pcn = ?",[$serial,$pcn]);
+if(sqlsrv_rows_affected($stmt)>=1){
 	header("Location: index.php?return=*SUCCESS. Item Returned*");
-	$stmt->close();
-	$conn->close();
+	sqlsrv_close($conn);
 	exit();
 }
 else{
 	header("Location: index.php?return=*FAILED to Return Item. Please Check the Serial or PCN again*");
-	$stmt->close();
-	$conn->close();
+	sqlsrv_close($conn);
 	exit();
 }
 ?>
